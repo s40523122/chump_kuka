9aaf1179488cd2adff2464e64857951f96519831
s40523122 <s40523122@gm.nfu.edu.tw>
2025-06-02 15:15:47 +0800

    增加縮小泡泡

--- CNCAppPlatform/Form1.cs
+++ CNCAppPlatform/Form1.cs
@@ -15,12 +15,14 @@ using Chump_kuka.Controls;
 using Chump_kuka.Dispatchers;
 using Newtonsoft.Json;
 using Chump_kuka.Services;
+using System.Drawing.Drawing2D;
 
 namespace Chump_kuka
 {
     public partial class Form1 : iCAPS.Form1
     {
         private UdpChatRoom _udp_chat_room = new UdpChatRoom();
+        private Form circle_form;
         public Form1()
         {
             InitializeComponent();
@@ -33,9 +35,19 @@ namespace Chump_kuka
 
             modbusService = new ModbusTCPMasterManager();
             Load += Form1_Load;
+            SizeChanged += Form1_SizeChanged;
 
             //string binPath = Path.Combine(Application.StartupPath, "config\\layout.ini");
             //MessageBox.Show("Bin 資料夾路徑：" + binPath);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                circle_form.Show();
+                this.Hide();
+            }
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -54,6 +66,33 @@ namespace Chump_kuka
         {
             listDI = new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
             //listDO = new List<PictureBox> { DO0, DO1, DO2, DO3 };
+
+            // 設定圓形視窗
+            circle_form = new Form();
+            circle_form.StartPosition = FormStartPosition.Manual; // 手動設定位置
+            circle_form.FormBorderStyle = FormBorderStyle.None; // 去除邊框
+            circle_form.BackColor = Color.LightBlue;
+            circle_form.Width = 100;
+            circle_form.Height = 100;
+            circle_form.TopMost = true;     // 置於螢幕最上層
+            circle_form.Cursor = Cursors.Hand;
+            // 計算右下角位置
+            int x = Screen.PrimaryScreen.WorkingArea.Width - circle_form.Width;
+            int y = Screen.PrimaryScreen.WorkingArea.Height - circle_form.Height;
+            circle_form.Location = new Point(x, y);
+            // 修剪成圓形
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, circle_form.Width, circle_form.Height);
+            circle_form.Region = new Region(path);
+
+            circle_form.Click += Circle_form_Click;
+        }
+
+        private void Circle_form_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.Show();
+            circle_form.Hide();
         }
 
         private void callback(string msg)
