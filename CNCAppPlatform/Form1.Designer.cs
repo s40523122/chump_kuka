e80568803fdb51e012ef7708a68690812c7250ea
s40523122 <s40523122@gm.nfu.edu.tw>
2025-06-18 16:14:20 +0800

    清理Form1垃圾

--- CNCAppPlatform/Form1.Designer.cs
+++ CNCAppPlatform/Form1.Designer.cs
@@ -30,42 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.enable_api_btn = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.btStart = new System.Windows.Forms.Button();
-            this.btStop = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button2 = new System.Windows.Forms.Button();
             this.open_log_button = new System.Windows.Forms.CheckBox();
             this.btnUdpLog = new System.Windows.Forms.Button();
             this.logWindow1 = new Chump_kuka.Controls.LogWindow();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.logWindow1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.btStop);
-            this.panel1.Controls.Add(this.btStart);
-            this.panel1.Controls.Add(this.pictureBox4);
-            this.panel1.Controls.Add(this.pictureBox3);
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Size = new System.Drawing.Size(959, 689);
-            this.panel1.Controls.SetChildIndex(this.pictureBox1, 0);
-            this.panel1.Controls.SetChildIndex(this.pictureBox2, 0);
-            this.panel1.Controls.SetChildIndex(this.pictureBox3, 0);
-            this.panel1.Controls.SetChildIndex(this.pictureBox4, 0);
-            this.panel1.Controls.SetChildIndex(this.btStart, 0);
-            this.panel1.Controls.SetChildIndex(this.btStop, 0);
-            this.panel1.Controls.SetChildIndex(this.button2, 0);
             this.panel1.Controls.SetChildIndex(this.logWindow1, 0);
             this.panel1.Controls.SetChildIndex(this.enable_side, 0);
             // 
@@ -78,83 +52,6 @@
             this.enable_api_btn.Text = "enable api";
             this.enable_api_btn.UseVisualStyleBackColor = true;
             this.enable_api_btn.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox1.Location = new System.Drawing.Point(86, 194);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(56, 56);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox2.Location = new System.Drawing.Point(165, 194);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(56, 56);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Visible = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox3.Location = new System.Drawing.Point(242, 194);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(56, 56);
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Visible = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox4.Location = new System.Drawing.Point(320, 194);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(56, 56);
-            this.pictureBox4.TabIndex = 1;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Visible = false;
-            // 
-            // btStart
-            // 
-            this.btStart.Location = new System.Drawing.Point(166, 272);
-            this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(91, 45);
-            this.btStart.TabIndex = 2;
-            this.btStart.Text = "start";
-            this.btStart.UseVisualStyleBackColor = true;
-            this.btStart.Visible = false;
-            this.btStart.Click += new System.EventHandler(this.btStart_Click);
-            // 
-            // btStop
-            // 
-            this.btStop.Location = new System.Drawing.Point(285, 272);
-            this.btStop.Name = "btStop";
-            this.btStop.Size = new System.Drawing.Size(91, 45);
-            this.btStop.TabIndex = 2;
-            this.btStop.Text = "stop";
-            this.btStop.UseVisualStyleBackColor = true;
-            this.btStop.Visible = false;
-            this.btStop.Click += new System.EventHandler(this.btStop_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(708, 411);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 42);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // open_log_button
             // 
@@ -203,10 +100,6 @@
             this.Controls.SetChildIndex(this.btnUdpLog, 0);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,14 +108,6 @@
         #endregion
 
         private System.Windows.Forms.Button enable_api_btn;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btStop;
-        private System.Windows.Forms.Button btStart;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button2;
         private Controls.LogWindow logWindow1;
         private System.Windows.Forms.CheckBox open_log_button;
         private System.Windows.Forms.Button btnUdpLog;
--- CNCAppPlatform/Form1.cs
+++ CNCAppPlatform/Form1.cs
@@ -33,7 +33,6 @@ namespace Chump_kuka
             _udp_chat_room.Show();
             _udp_chat_room.Hide();
 
-            modbusService = new ModbusTCPMasterManager();
             Load += Form1_Load;
             SizeChanged += Form1_SizeChanged;
 
@@ -57,115 +56,9 @@ namespace Chump_kuka
             //KukaApiController.Enable = true;
         }
 
-
-        private ModbusTCPMasterManager modbusService;
-        private bool NetworkIsOk = false;
-        private List<PictureBox> listDI;
-        private List<PictureBox> listDO;
-
         private async void Form1_Load(object sender, EventArgs e)
         {
-            listDI = new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
-            //listDO = new List<PictureBox> { DO0, DO1, DO2, DO3 };
 
-        }
-
-        private async void btStart_Click(object sender, EventArgs e)
-        {
-            // NetworkIsOk = modbusService.Connect(txtIP.Text);
-            NetworkIsOk = await modbusService.Connect("169.254.64.100");     // "192.168.255.1"
-            if (NetworkIsOk)
-            {
-                timer1.Interval = 1000;
-                timer1.Enabled = true;
-                btStart.Enabled = false;
-                btStop.Enabled = true;
-            }
-        }
-
-        private void btStop_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = false;
-            modbusService.Disconnect();
-            btStart.Enabled = true;
-            btStop.Enabled = false;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (NetworkIsOk)
-            {
-                UpdateDI();
-                //UpdateDO();
-            }
-        }
-
-        private void UpdateDI()
-        {
-            bool[] status = modbusService.ReadDI(listDI.Count);
-            for (int i = 0; i < listDI.Count; i++)
-            {
-                listDI[i].BackColor = status[i] ? Color.Lime : Color.Green;
-            }
-        }
-
-        private void UpdateDO()
-        {
-            bool[] coils = modbusService.ReadDO(listDO.Count);
-            for (int i = 0; i < listDO.Count; i++)
-            {
-                listDO[i].BackColor = coils[i] ? Color.Red : Color.Maroon;
-            }
-        }
-
-        private void DO_Click(object sender, EventArgs e)
-        {
-            int index = int.Parse(((PictureBox)sender).Tag.ToString());
-            bool currentState = listDO[index].BackColor == Color.Red;
-            modbusService.WriteDO(index, !currentState);
-            listDO[index].BackColor = !currentState ? Color.Red : Color.Maroon;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Log.Append("這是一個測試訊息", "Test", "Form1");
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            JArray robot_infos = (JArray)JObject.Parse($@"{{""code"": ""0"", 
-                                                                     ""message"": """", 
-                                                                      ""success"": true, 
-                                                                      ""data"":[{{ 
-                                                                            ""robotId"":""robot001"", 
-                                                                            ""robotType"":""test001"", 
-                                                                            ""containerCode"" : ""container001"", 
-                                                                            ""mapCode"" : ""map01"", 
-                                                                            ""floorNumber"" : ""floor01"", 
-                                                                            ""buildingCode"" : ""9001"", 
-                                                                            ""status"" : 4, 
-                                                                            ""occupyStatus"" : 1, 
-                                                                            ""batteryLevel"" : 60, 
-                                                                            ""nodeCode"" : ""node001"", 
-                                                                            ""missionCode"" : ""missionCode111"", 
-                                                                            ""updateTime"" : ""{DateTime.Now.ToString(@"G")}""
-                                                                           }},
-                                                                           {{ 
-                                                                            ""robotId"":""robot002"", 
-                                                                            ""robotType"":""test002"", 
-                                                                            ""containerCode"" : ""container002"", 
-                                                                            ""mapCode"" : ""map02"", 
-                                                                            ""floorNumber"" : ""floor02"", 
-                                                                            ""buildingCode"" : ""9002"", 
-                                                                            ""status"" : 4, 
-                                                                            ""occupyStatus"" : 1, 
-                                                                            ""batteryLevel"" : 60, 
-                                                                            ""nodeCode"" : ""node001"", 
-                                                                            ""missionCode"" : ""missionCode111"", 
-                                                                            ""updateTime"" : ""{DateTime.Now.ToString(@"G")}""
-                                                                           }}] 
-                                                                    }} ")["data"];
-        KukaParm.RobotStatusInfos = robot_infos;
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
